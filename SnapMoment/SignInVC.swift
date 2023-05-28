230d1650b2db9c6f31c0318f14bf5ab56567b924
azimgunes <azimgunesofficial@gmail.com>
2023-05-29 00:40:51 +0300

    The SnapMoment project was created.

---
+++ SnapMoment/SignInVC.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  SnapMoment
+//
+//  Created by Azim Güneş on 26.05.2023.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    
+    
+    @IBOutlet weak var mailTextField: UITextField!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SignInButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func RegisterButton(_ sender: Any) {
+    }
+    
+    @IBAction func GoogleButton(_ sender: Any) {
+    }
+    
+    
+
+}
+
