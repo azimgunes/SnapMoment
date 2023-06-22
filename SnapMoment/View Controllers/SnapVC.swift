4e40cc539eaf1a4449e363db31b0c612b600ff35
azimgunes <azimgunesofficial@gmail.com>
2023-06-22 18:38:35 +0300

    Snaps pulled from Firebase.

--- SnapMoment/View Controllers/SnapVC.swift
+++ SnapMoment/View Controllers/SnapVC.swift
@@ -6,27 +6,48 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class SnapVC: UIViewController {
     
+    @IBOutlet weak var scrolButton: UIImageView!
     
     @IBOutlet weak var timeLabel: UILabel!
     
     var selectedSnap : Snap?
     var selectedTime : Int?
+    var inputArray = [SDWebImageSource]()
     
 
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+    
+    
+
         if let time = selectedTime {
             timeLabel.text! = "\(time) Saat Sonra Kaybolacak."
 
         }
-        
-        super.viewDidLoad()
+        if let snap = selectedSnap {
+            for imageUrl in snap.imageUrlArray {
+                inputArray.append(SDWebImageSource(urlString: imageUrl)!)
+                            }
+            let imageSlideShow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width*0.95, height: self.view.frame.height*0.90 ))
+            imageSlideShow.backgroundColor = UIColor.clear
+            
+            let pageInd = UIPageControl()
+            pageInd.currentPageIndicatorTintColor = UIColor.lightGray
+            pageInd.pageIndicatorTintColor = UIColor.black
+            imageSlideShow.pageIndicator = pageInd
+            
+            
+            
+            imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFit
+            imageSlideShow.setImageInputs(inputArray)
+            self.view.addSubview(imageSlideShow)
+            self.view.bringSubviewToFront(timeLabel)
+        }
 
     }
-    
-
 
 }
